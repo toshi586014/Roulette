13e7355ffa31a7fd08fd0b1eedb3078c5a572c08
toshi586014 <toshi586014@icloud.com>
2016-09-20 18:04:21 +0900

    Initial Commit

---
+++ Roulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roulette
+//
+//  Created by Toshiaki Nakamura on 2016/09/20.
+//  Copyright © 2016年 Toshiaki Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
